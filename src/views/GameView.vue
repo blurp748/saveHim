d70c613e6d03df1b65c6967d85acbb032b6baed5
blurp748 <mathistanghe@hotmail.fr>
2022-09-15 09:35:09 +0200

    [ 🛠 ] Fix keyboard responsive

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -16,37 +16,37 @@
             <div class="m-5" v-for="lesson in findLettersRef">{{lesson}}</div>
         </div>
         <div class="mt-10">
-            <div class="flex justify-center gap-4 my-3 w-full">
-                <kbd @click="verifyLetter('q',16)" class="kbd">{{alphabetRef[16]}}</kbd>
-                <kbd @click="verifyLetter('w',22)" class="kbd">{{alphabetRef[22]}}</kbd>
-                <kbd @click="verifyLetter('e',4)" class="kbd">{{alphabetRef[4]}}</kbd>
-                <kbd @click="verifyLetter('r',17)" class="kbd">{{alphabetRef[17]}}</kbd>
-                <kbd @click="verifyLetter('t',19)" class="kbd">{{alphabetRef[19]}}</kbd>
-                <kbd @click="verifyLetter('y',24)" class="kbd">{{alphabetRef[24]}}</kbd>
-                <kbd @click="verifyLetter('u',20)" class="kbd">{{alphabetRef[20]}}</kbd>
-                <kbd @click="verifyLetter('i',8)" class="kbd">{{alphabetRef[8]}}</kbd>
-                <kbd @click="verifyLetter('o',14)" class="kbd">{{alphabetRef[14]}}</kbd>
-                <kbd @click="verifyLetter('p',15)" class="kbd">{{alphabetRef[15]}}</kbd>
+            <div class="flex justify-center gap-2 my-3 w-full">
+                <kbd @click="verifyLetter('q',16)" class="kbd kbd-sm md:kbd-md lg:kbd-lg">{{alphabetRef[16]}}</kbd>
+                <kbd @click="verifyLetter('w',22)" class="kbd kbd-sm md:kbd-md lg:kbd-lg">{{alphabetRef[22]}}</kbd>
+                <kbd @click="verifyLetter('e',4)" class="kbd kbd-sm md:kbd-md lg:kbd-lg">{{alphabetRef[4]}}</kbd>
+                <kbd @click="verifyLetter('r',17)" class="kbd kbd-sm md:kbd-md lg:kbd-lg">{{alphabetRef[17]}}</kbd>
+                <kbd @click="verifyLetter('t',19)" class="kbd kbd-sm md:kbd-md lg:kbd-lg">{{alphabetRef[19]}}</kbd>
+                <kbd @click="verifyLetter('y',24)" class="kbd kbd-sm md:kbd-md lg:kbd-lg">{{alphabetRef[24]}}</kbd>
+                <kbd @click="verifyLetter('u',20)" class="kbd kbd-sm md:kbd-md lg:kbd-lg">{{alphabetRef[20]}}</kbd>
+                <kbd @click="verifyLetter('i',8)" class="kbd kbd-sm md:kbd-md lg:kbd-lg">{{alphabetRef[8]}}</kbd>
+                <kbd @click="verifyLetter('o',14)" class="kbd kbd-sm md:kbd-md lg:kbd-lg">{{alphabetRef[14]}}</kbd>
+                <kbd @click="verifyLetter('p',15)" class="kbd kbd-sm md:kbd-md lg:kbd-lg">{{alphabetRef[15]}}</kbd>
             </div> 
-            <div class="flex justify-center gap-4 my-3 w-full">
-                <kbd @click="verifyLetter('a',0)" class="kbd">{{alphabetRef[0]}}</kbd>
-                <kbd @click="verifyLetter('s',18)" class="kbd">{{alphabetRef[18]}}</kbd>
-                <kbd @click="verifyLetter('d',3)" class="kbd">{{alphabetRef[3]}}</kbd>
-                <kbd @click="verifyLetter('f',5)" class="kbd">{{alphabetRef[5]}}</kbd>
-                <kbd @click="verifyLetter('g',6)" class="kbd">{{alphabetRef[6]}}</kbd>
-                <kbd @click="verifyLetter('h',7)" class="kbd">{{alphabetRef[7]}}</kbd>
-                <kbd @click="verifyLetter('j',9)" class="kbd">{{alphabetRef[9]}}</kbd>
-                <kbd @click="verifyLetter('k',10)" class="kbd">{{alphabetRef[10]}}</kbd>
-                <kbd @click="verifyLetter('l',11)" class="kbd">{{alphabetRef[11]}}</kbd>
+            <div class="flex justify-center gap-2 my-3 w-full">
+                <kbd @click="verifyLetter('a',0)" class="kbd kbd-sm md:kbd-md lg:kbd-lg">{{alphabetRef[0]}}</kbd>
+                <kbd @click="verifyLetter('s',18)" class="kbd kbd-sm md:kbd-md lg:kbd-lg">{{alphabetRef[18]}}</kbd>
+                <kbd @click="verifyLetter('d',3)" class="kbd kbd-sm md:kbd-md lg:kbd-lg">{{alphabetRef[3]}}</kbd>
+                <kbd @click="verifyLetter('f',5)" class="kbd kbd-sm md:kbd-md lg:kbd-lg">{{alphabetRef[5]}}</kbd>
+                <kbd @click="verifyLetter('g',6)" class="kbd kbd-sm md:kbd-md lg:kbd-lg">{{alphabetRef[6]}}</kbd>
+                <kbd @click="verifyLetter('h',7)" class="kbd kbd-sm md:kbd-md lg:kbd-lg">{{alphabetRef[7]}}</kbd>
+                <kbd @click="verifyLetter('j',9)" class="kbd kbd-sm md:kbd-md lg:kbd-lg">{{alphabetRef[9]}}</kbd>
+                <kbd @click="verifyLetter('k',10)" class="kbd kbd-sm md:kbd-md lg:kbd-lg">{{alphabetRef[10]}}</kbd>
+                <kbd @click="verifyLetter('l',11)" class="kbd kbd-sm md:kbd-md lg:kbd-lg">{{alphabetRef[11]}}</kbd>
             </div> 
-            <div class="flex justify-center gap-4 my-3 w-full">
-                <kbd @click="verifyLetter('z',25)" class="kbd">{{alphabetRef[25]}}</kbd>
-                <kbd @click="verifyLetter('x',23)" class="kbd">{{alphabetRef[23]}}</kbd>
-                <kbd @click="verifyLetter('c',2)" class="kbd">{{alphabetRef[2]}}</kbd>
-                <kbd @click="verifyLetter('v',21)" class="kbd">{{alphabetRef[21]}}</kbd>
-                <kbd @click="verifyLetter('b',1)" class="kbd">{{alphabetRef[1]}}</kbd>
-                <kbd @click="verifyLetter('n',13)" class="kbd">{{alphabetRef[13]}}</kbd>
-                <kbd @click="verifyLetter('m',12)" class="kbd">{{alphabetRef[12]}}</kbd>
+            <div class="flex justify-center gap-2 my-3 w-full">
+                <kbd @click="verifyLetter('z',25)" class="kbd kbd-sm md:kbd-md lg:kbd-lg">{{alphabetRef[25]}}</kbd>
+                <kbd @click="verifyLetter('x',23)" class="kbd kbd-sm md:kbd-md lg:kbd-lg">{{alphabetRef[23]}}</kbd>
+                <kbd @click="verifyLetter('c',2)" class="kbd kbd-sm md:kbd-md lg:kbd-lg">{{alphabetRef[2]}}</kbd>
+                <kbd @click="verifyLetter('v',21)" class="kbd kbd-sm md:kbd-md lg:kbd-lg">{{alphabetRef[21]}}</kbd>
+                <kbd @click="verifyLetter('b',1)" class="kbd kbd-sm md:kbd-md lg:kbd-lg">{{alphabetRef[1]}}</kbd>
+                <kbd @click="verifyLetter('n',13)" class="kbd kbd-sm md:kbd-md lg:kbd-lg">{{alphabetRef[13]}}</kbd>
+                <kbd @click="verifyLetter('m',12)" class="kbd kbd-sm md:kbd-md lg:kbd-lg">{{alphabetRef[12]}}</kbd>
             </div>
         </div>
         <div class="flex items-center justify-center gap-10 mt-10">
